94f6ead9a3f3d40cb6c34f0730819500c98a0986
anabsena <anabsenaf@gmail.com>
2024-02-21 22:48:48 -0300

    tentativa 01

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View >
+      <View >
+        <Text >OI CARAIO</Text>
+      </View>
+    </View>
+  );
+}
+
